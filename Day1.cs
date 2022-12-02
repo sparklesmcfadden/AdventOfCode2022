2517e824adab7c9674033c834a63158013a6a456
cavanfarrell <cavan.farrell@icloud.com>
2022-12-01 21:39:42 -0600

    Initial commit

---
+++ Day1.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode2022;
+
+public class Day1
+{
+    private static List<int> GetTotals()
+    {
+        var file = Utilities.LoadFile("data/day1_part1.txt");
+
+        return file.Split("\n\n")
+            .Select(m => m.Split("\n").Select(i => Int32.Parse(i)).Sum())
+            .ToList();
+    }
+
+    public int Part1()
+    {
+        return GetTotals().Max();
+    }
+
+    public int Part2()
+    {
+        var totals = GetTotals();
+
+        totals.Sort();
+        return totals.TakeLast(3).Sum();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode2022;
+
+var day1 = new Day1();
+Console.WriteLine(day1.Part1());
+Console.WriteLine(day1.Part2());
